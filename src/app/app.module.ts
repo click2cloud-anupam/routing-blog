6919a522d07c03d7cd73baa1b812260aa313d05c
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-05-25 12:45:56 +0530

    performed child routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NoPageComponent } from './no-page/no-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NoPageComponent } from './no-page/no-page.component';
     UserComponent,
     HomeComponent,
     AboutComponent,
-    NoPageComponent
+    NoPageComponent,
+    AboutMeComponent,
+    AboutCompanyComponent
   ],
   imports: [
     BrowserModule,
